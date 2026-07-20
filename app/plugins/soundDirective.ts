d6c14e864f839793772f36ce2afb64ad1296df8f
Vishnu_Mac <thenormvg@gmail.com>
2026-07-20 22:26:33 +0530

    feat: add .hover modifier to v-sound directive and apply to major interactive cards

--- app/plugins/soundDirective.ts
+++ app/plugins/soundDirective.ts
@@ -8,21 +8,33 @@ export default defineNuxtPlugin((nuxtApp) => {
   // Usage: v-sound="'click'" or v-sound="{ type: 'pop', feel: 'glass' }"
   nuxtApp.vueApp.directive('sound', {
     mounted(el, binding) {
-      el.addEventListener('click', () => {
-        let soundType = 'click'
-        let soundFeel = 'aero'
-        
-        if (binding.value) {
-          if (typeof binding.value === 'string') {
-            soundType = binding.value
-          } else if (typeof binding.value === 'object') {
-            soundType = binding.value.type || 'click'
-            soundFeel = binding.value.feel || 'aero'
+      const attachSound = (event) => {
+        el.addEventListener(event, () => {
+          let soundType = event === 'mouseenter' ? 'tick' : 'click'
+          let soundFeel = 'aero'
+          
+          if (binding.value) {
+            if (typeof binding.value === 'string') {
+              soundType = binding.value
+            } else if (typeof binding.value === 'object') {
+              soundType = binding.value.type || soundType
+              soundFeel = binding.value.feel || 'aero'
+            }
           }
-        }
-        
-        playSound(soundType, soundFeel)
-      })
+          
+          playSound(soundType, soundFeel)
+        })
+      }
+
+      // Default to click if no modifiers provided, or if .click is explicitly used
+      if (binding.modifiers.click || Object.keys(binding.modifiers).length === 0) {
+        attachSound('click')
+      }
+      
+      // Support .hover modifier
+      if (binding.modifiers.hover) {
+        attachSound('mouseenter')
+      }
     },
     getSSRProps() {
       return {} // Return empty object to prevent SSR crash
